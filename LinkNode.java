b76c665baf67fa09b3ca32a059b9b050cebcf551
Michael Woo <michaelwoo36@gmail.com>
2023-01-31 19:25:34 -0800

    Final

--- LinkNode.java
+++ LinkNode.java
@@ -1,13 +1,20 @@
 import java.util.LinkedList;
 
+// One Node of linkedList. Four data values.
 public class LinkNode {
 
     public LinkNode next;
+
     public String name;
+
     public String phone;
+
     public String address;
+
     public String city;
+
     public LinkNode(String name, String phone, String address, String city){
+
         this.name = name;
         this.phone = phone;
         this.address = address;
