dec9c2cecc4d51a04c861a73cddbb49e0bbeedab
Michael Woo <michaelwoo36@gmail.com>
2023-01-31 18:31:40 -0800

    Works

--- Main.java
+++ Main.java
@@ -7,57 +7,33 @@ public class Main {
         PhoneBookManager book = new PhoneBookManager(input);
         boolean ask = true;
         while(ask) {
-            System.out.println("add contact (a)");
-            System.out.println("remove contact (r)");
-            System.out.println("change contact (c)");
-            System.out.println("Display phone book (d)");
+
+            book.ask();
 
             String ans = input.next();
 
-            if (ans.equals("a")){
-                System.out.println("Name");
-                String name = input.next();
-                System.out.println("Phone");
-                String phone = input.next();
-                System.out.println("address");
-                String address = input.next();
-                System.out.println("city");
-                String city = input.next();
-                book.add(name, phone, address, city);
-            }
-            else if(ans.equals("r")){
-                System.out.println("Type name, phone, address, and city of contact you want to remove");
-                System.out.println("Name");
-                String name = input.next();
-                System.out.println("Phone");
-                String phone = input.next();
-                System.out.println("address");
-                String address = input.next();
-                System.out.println("city");
-                String city = input.next();
-                book.remove(name, phone, address, city);
+            switch (ans) {
+                case "a":
+                    book.add();
+                    break;
+                case "r":
+                    book.remove();
+                    break;
+                case "c":
+                    book.change();
+                    break;
 
-            }
-            else if(ans.equals("c")){
-                System.out.println("Type name, phone, address, and city of contact you want to change");
-                System.out.println("Name");
-                String name = input.next();
-                System.out.println("Phone");
-                String phone = input.next();
-                System.out.println("address");
-                String address = input.next();
-                System.out.println("city");
-                String city = input.next();
-                book.change(name, phone, address, city);
-                book.printBook();
-            }
+                case "d":
+                    book.printBook();
+                    break;
+                case "e":
+                    ask = false;
+                    break;
+                default:
+                    System.out.println();
+                    System.out.println("Not an option. Please enter one of the operational keys keys");
+                    System.out.println();
 
-            else if(ans.equals("d")){
-                book.printBook();
-            }
-
-            else{
-                break;
             }
         }
 
--- PhoneBookManager.java
+++ PhoneBookManager.java
@@ -3,7 +3,11 @@ import java.util.Scanner;
 
 public class PhoneBookManager {
     public LinkNode front;
-    Scanner input;
+    public Scanner input;
+    public String name;
+    public String phone;
+    public String address;
+    public String city;
 
 
     public PhoneBookManager(Scanner input) {
@@ -11,7 +15,37 @@ public class PhoneBookManager {
         front = null;
     }
 
-    public void add(String name, String phone, String address, String city) {
+    public void ask(){
+        System.out.println("add contact (a)");
+        System.out.println("remove contact (r)");
+        System.out.println("change contact (c)");
+        System.out.println("Display phone book (d)");
+        System.out.println("Close phone book (e)");
+    }
+
+    public void whichOne(){
+        System.out.println("Name");
+        input.nextLine();
+        name = input.nextLine();
+        System.out.println("Phone");
+        phone = input.nextLine();
+        System.out.println("address");
+        address = input.nextLine();
+        System.out.println("city");
+        city = input.nextLine();
+    }
+
+    public void showContact(LinkNode current){
+        System.out.println();
+        System.out.println("Name: " + current.name);
+        System.out.println("Phone: " + current.phone);
+        System.out.println("Address: " + current.address);
+        System.out.println("City: " + current.city);
+        System.out.println();
+    }
+
+    public void add() {
+        whichOne();
         LinkNode temp = front;
         LinkNode added;
         if(front == null){
@@ -27,70 +61,78 @@ public class PhoneBookManager {
 
         }
         System.out.println();
-        System.out.println("Added contact:");
-        System.out.println();
-        System.out.println("Name: " + added.name);
-        System.out.println("Phone: " + added.phone);
-        System.out.println("Address: " + added.address);
-        System.out.println("City: " + added.city);
-        System.out.println();
+        System.out.println("added contact:");
+        showContact(added);
 
     }
 
-    public void remove(String name, String phone, String address, String city){
-        LinkNode current = front;
-        LinkNode prev = null;
-        if (current.name.equals(name) && current.phone.equals(phone) && current.address.equals(address) && current.city.equals(city)) {
-            front = current.next;
-            return;
-        }
-        while(current != null && (!(current.name.equals(name)) || !(current.phone.equals(phone)) || !(current.address.equals(address)) || !(current.city.equals(city)))){
-            prev = current;
-            current = current.next;
-        }
-        prev.next = current.next;
-        System.out.println();
-        System.out.println("Contact removed.");
-    }
-
-    public void change(String name, String phone, String address, String city){
-        System.out.println("What do you want to change");
-        System.out.println("name (n), phone (p), address (a), or city (c)");
-        String ans = input.next();
-        LinkNode current = front;
-        while(current != null && (!(current.name.equals(name)) || !(current.phone.equals(phone)) || !(current.address.equals(address)) || !(current.city.equals(city)))){
-            current = current.next;
-        }
-        if(ans.equals("n")){
-            System.out.println("What would you like to change it to");
-            ans = input.next();
-            if (current.name.equals(name) && current.phone.equals(phone) && current.address.equals(address) && current.city.equals(city)) {
-                current.name = ans;
+    public void remove(){
+        System.out.println("Type name, phone, address, and city of contact you want to remove");
+        whichOne();
+        try {
+            LinkNode current = front;
+            LinkNode prev = null;
+            if (current.name.equalsIgnoreCase(name) && current.phone.equalsIgnoreCase(phone) && current.address.equalsIgnoreCase(address) && current.city.equalsIgnoreCase(city)) {
+                front = current.next;
+                return;
             }
-        }
-        else if (ans.equals("p")){
-            System.out.println("What would you like to change it to");
-            ans = input.next();
-            current.phone = ans;
-        }
-        else if (ans.equals("a")){
-            System.out.println("What would you like to change it to");
-            ans = input.next();
-            current.address = ans;
-        }
-        else if (ans.equals("c")){
-            System.out.println("What would you like to change it to");
-            ans = input.next();
-            current.city = ans;
+            while (current != null && (!(current.name.equalsIgnoreCase(name)) || !(current.phone.equalsIgnoreCase(phone)) || !(current.address.equalsIgnoreCase(address)) || !(current.city.equalsIgnoreCase(city)))) {
+                prev = current;
+                current = current.next;
+            }
+            prev.next = current.next;
             System.out.println();
-            System.out.println("Changed contact:");
+            System.out.println("Contact removed.");
+        }
+        catch (NullPointerException nullPoint){
             System.out.println();
-            System.out.println("Name: " + current.name);
-            System.out.println("Phone: " + current.phone);
-            System.out.println("Address: " + current.address);
-            System.out.println("City: " + current.city);
+            System.out.println("The contact you wish to remove does not exist");
             System.out.println();
         }
+    }
+
+    public void change(){
+        System.out.println("Type name, phone, address, and city of contact you want to change");
+        whichOne();
+        try {
+            System.out.println("What do you want to change");
+            System.out.println("name (n), phone (p), address (a), or city (c)");
+            String ans = input.next();
+            LinkNode current = front;
+            while (current != null && (!(current.name.equals(name)) || !(current.phone.equals(phone)) || !(current.address.equals(address)) || !(current.city.equals(city)))) {
+                current = current.next;
+            }
+            if (ans.equals("n")) {
+                System.out.println("What would you like to change it to");
+                input.nextLine();
+                ans = input.nextLine();
+                if (current.name.equals(name) && current.phone.equals(phone) && current.address.equals(address) && current.city.equals(city)) {
+                    current.name = ans;
+                }
+            } else if (ans.equals("p")) {
+                System.out.println("What would you like to change it to");
+                input.nextLine();
+                ans = input.nextLine();
+                current.phone = ans;
+            } else if (ans.equals("a")) {
+                System.out.println("What would you like to change it to");
+                input.nextLine();
+                ans = input.nextLine();
+                current.address = ans;
+            } else if (ans.equals("c")) {
+                input.nextLine();
+                System.out.println("What would you like to change it to");
+                ans = input.nextLine();
+                current.city = ans;
+            }
+            showContact(current);
+        }
+        catch(NullPointerException nullPoint){
+            System.out.println();
+            System.out.println("Contact you wish to change does not exist");
+            System.out.println();
+        }
+
     }
 
     public void printBook() {
@@ -98,6 +140,12 @@ public class PhoneBookManager {
         System.out.println("Phone Book:");
         System.out.println();
         LinkNode current = front;
+
+        if(current == null){
+            System.out.println("No contacts in Phone Book");
+            System.out.println();
+        }
+
         while(current != null){
             System.out.println("Name: " + current.name);
             System.out.println("Phone: " + current.phone);
